217378a5a65f723ec95d12fffd8cf9385d11890e
raphael <moudens.raphael@orange.fr>
2017-06-09 16:09:47 +0200

    modif fichier

--- gulpfile.js
+++ gulpfile.js
@@ -9,38 +9,38 @@ var scsslint = require('gulp-scss-lint');
 
 gulp.task('sass', function() {
   return gulp.src('assets/sass/app.scss')
-    .pipe($.sass({
-      includePaths: 'assets/sass/vendor',
+  .pipe($.sass({
+    includePaths: 'assets/sass/vendor',
 
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
+  })
+  .on('error', $.sass.logError))
+    // .pipe($.autoprefixer({
+    //   browsers: ['last 2 versions', 'ie >= 9']
+    // }))
     .pipe(gulp.dest('public/css'))
     .pipe(browserSync.stream());
-});
+  });
 
 gulp.task('uglify', function (cb) {
   pump([
-        gulp.src('assets/js/app.js'),
-        uglify(),
-        gulp.dest('public/js/')
+    gulp.src('assets/js/app.js'),
+    uglify(),
+    gulp.dest('public/js/')
     ],
     cb
-  ).pipe(browserSync.stream());
+    ).pipe(browserSync.stream());
 });
 
 
 gulp.task('serve', ['sass', 'uglify'], function() {
-    browserSync.init({
-        server: './public/',
-        open: true,
-        notify: false
-    });
-		gulp.watch('assets/sass/**/*.scss',['sass']);
-		gulp.watch('assets/js/**/*.js', ['uglify']);
-		 gulp.watch("public/*.html").on('change', browserSync.reload);
+  browserSync.init({
+    server: './public/',
+    open: true,
+    notify: false
+  });
+  gulp.watch('assets/sass/**/*.scss',['sass']);
+  gulp.watch('assets/js/**/*.js', ['uglify']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
